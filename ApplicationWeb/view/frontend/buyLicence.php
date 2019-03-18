dbda46dd0710b000d320bd298e57531906fded97
Adrien ALARY <adrien.alary@limayrac.fr>
2019-03-18 19:00:57 +0100

    Ajout boutons paiement paypal

--- ApplicationWeb/view/frontend/buyLicence.php
+++ ApplicationWeb/view/frontend/buyLicence.php
@@ -8,27 +8,30 @@ $styleMenu = '"mainAchat"';
 
 <section class="main styleAchat">
     <div class="container">
-        <div> 
+        
 			<?php
 			while ($data = $listById->fetch())
 			{
 			?>	
 				<p class="linkToPay">
 					Vous avec opté pour la licence <?php echo $data["Libelle"];?> dont le montant total à payer est de <?php echo $data["Prix"];?> € </br>
-					Par quel moyen de paiement souahitez-vous régler votre achat ?
+					Par quel moyen de paiement souhaitez-vous régler votre achat ?
 				</p>
-				<a class="linkToPay" href="https://www.paypal.com/fr/webapps/mpp/home">
-					<img src="public/images/paypal.png" alt="Paiement par paypal"/>
-				</a>
-				<a class="linkToPay" href="https://hipay.com/fr">
-					<img src="public/images/hipay.png" alt="Paiement par Hipay"/>
-				</a>
 
+				<paypal>
+					<script src="https://www.paypal.com/sdk/js?client-id=sb"></script>
+					<script>paypal.Buttons().render('paypal');</script>
+				</paypal>
+				<hipay>
+					<a class="linkToPay" href="https://hipay.com/fr">
+						<img src="public/images/hipay.png" alt="Paiement par Hipay"/>
+					</a>
+				</hipay>
+				
 			<?php
 			}
 			$listById->closeCursor();
 			?>
-		</div>
 	</div>
 </section>
 <?php $content = ob_get_clean(); ?>
